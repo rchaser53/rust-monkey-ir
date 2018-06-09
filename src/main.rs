984ec160c0c65285252faa5afc389737d7e18d17
rchaser53 <tayoshizawa29@gmail.com>
2018-06-09 16:03:46 +0900

    try to use deserialize and keys

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ extern crate clap;
 extern crate reqwest;
 extern crate regex;
 extern crate serde_json;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
 
 #[macro_use]
 extern crate lazy_static;
@@ -30,10 +34,75 @@ lazy_static! {
       m
     };
 }
+ 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ItemsWrapper {
+    #[serde(with = "items")]
+    pub items: HashMap<i64, Items>,
+}
+ 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Items {
+    id: i64,
+    info: String,
+}
+
+mod items {
+    use super::Items;
+
+    use std::collections::HashMap;
+
+    use serde::ser::Serializer;
+    use serde::de::{Deserialize, Deserializer};
+
+    pub fn serialize<S>(map: &HashMap<i64, Items>, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.collect_seq(map.values())
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<HashMap<i64, Items>, D::Error>
+        where D: Deserializer<'de>
+    {
+        let mut map = HashMap::new();
+        for item in Vec::<Items>::deserialize(deserializer)? {
+            map.insert(item.id, item);
+        }
+        Ok(map)
+    }
+}
 
 fn main() {
-  println!("{}", attach_slash_initials("abc/def/"));
-  println!("{}", attach_slash_initials("/abc/def/"));
+  // println!("{}", attach_slash_initials("abc/def/"));
+  // println!("{}", attach_slash_initials("/abc/def/"));
+  // println!("{:?}", JSON_HASH["abc"]);
+
+  // for a in JSON_HASH["abc"].as_array().unwrap() {
+  //   println!("{:?}", a);
+  // }
+
+
+    let j = r#" {
+                  "items": [
+                    {
+                      "id": 3,
+                      "info": "three"
+                    },
+                    {
+                      "id": 2,
+                      "info": "two"
+                    }
+                  ]
+                } "#;
+
+    // println!("{:#?}", serde_json::from_str::<ItemsWrapper>(j).unwrap());
+
+    let json_obj = serde_json::from_str::<ItemsWrapper>(j).unwrap();
+
+    for key in json_obj.items.keys() {
+        println!("{:?}", json_obj.items[key]);
+    }
+
 }
 
 fn attach_slash_initials(target_str: &str) -> String {
