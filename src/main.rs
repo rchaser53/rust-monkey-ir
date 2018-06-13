ad3a8d7282e169280a2554770bd9fb39cf443532
rchaser53 <tayoshizawa29@gmail.com>
2018-06-13 18:45:18 +0900

    try to use where and Fn

--- src/main.rs
+++ src/main.rs
@@ -46,30 +46,50 @@ fn attach_slash_initials(target_str: &str) -> String {
   return "/".to_owned() + target_str;
 }
 
+
+fn hoge<T, F, S>(x : &T, f : F) -> S
+    where F : Fn(&T) -> S {
+    f(x)
+}
+
+fn test(v: &Vec<u16>) -> Vec<u32> {
+  v.into_iter().map(|new_v| {
+    let a = *new_v as u32;
+    a
+  }).collect()
+}
+
+fn main() {
+    let v = vec![0, 1, 2];
+    let aaa = hoge(&v, test);
+
+    println!("{:?}", aaa);
+}
+
   // let separeteds: Vec<&str> = url_string.split("/").collect();
   // for s in separeteds {
   //   println!("{}", s);
   // }
-fn main() {
-  let client = reqwest::Client::new();
-  let status = client
-      .request(reqwest::Method::Options, "http://localhost:3000/test")
-      .send()
-      .map(|res| res.status())
-      .map_err(|err| panic!(err));
+// fn main() {
+  // let client = reqwest::Client::new();
+  // let status = client
+  //     .request(reqwest::Method::Options, "http://localhost:3000/test")
+  //     .send()
+  //     .map(|res| res.status())
+  //     .map_err(|err| panic!(err));
   
-  if status.unwrap() == reqwest::StatusCode::Ok {
-    let res = client.put("http://localhost:3000/test")
-          .body("nyan")
-          .send();
+  // if status.unwrap() == reqwest::StatusCode::Ok {
+  //   let res = client.put("http://localhost:3000/test")
+  //         .body("nyan")
+  //         .send();
 
-    println!("{:?}", res.unwrap().text());
-  }
+  //   println!("{:?}", res.unwrap().text());
+  // }
 
 
   // let json = convert_str_json(&body).unwrap();
   // println!("{:?}", res);
-}
+// }
  // let body = reqwest::get("https://api.binance.com/api/v1/ticker/24hr?symbol=XRPBTC").unwrap()
   //                     .text().unwrap();
 
