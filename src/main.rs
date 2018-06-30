80b0f2f499e9e7475cc1c955539446f46b0dd560
rchaser53 <tayoshizawa29@gmail.com>
2018-06-30 11:15:16 +0900

    Rc::get_mut return immutable field

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,25 @@ extern crate serde_json;
 
 use std::rc::{Rc, Weak};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum AstType {
   Start,
   End,
   Normal,
 }
 
-#[derive(Debug)]
-struct Part {
+#[derive(Debug, Clone)]
+struct Part<'a> {
   start: usize,
   end: usize,
   kind: AstType,
   value: char,
-  children: Vec<Rc<Part>>,
-  parent: Weak<Part>,
+  children: Vec<Rc<Part<'a>>>,
+  parent: Weak<&'a Part<'a>>,
 }
 
-impl Part {
-  fn new(kind: AstType , imput: char, start: usize) -> Part {
+impl <'a>Part<'a> {
+  fn new(kind: AstType , imput: char, start: usize) -> Part<'a> {
     return Part {
       start: start,
       end: 0,
@@ -34,28 +34,30 @@ impl Part {
     }
   }
 
-  fn add_child(mut self, kind: AstType , imput: char, start: usize) -> Part {
-    return Part {
+  // fn add_child(mut self, kind: AstType , imput: char, start: usize, parent: &'a mut Part) -> Part<'a> {
+  fn add_child(mut self, kind: AstType , imput: char, start: usize, parent: &'a mut Part<'a>) {
+    let child = Rc::new(Part {
       start: start,
       end: 0,
       kind: kind,
       value: imput,
       children: Vec::new(),
-      parent: Rc::downgrade(&Rc::new(self)),
-    }
+      parent: Rc::downgrade(&Rc::new(parent)),
+    });
+    self.children.push(child);
   }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum WalkingType {
   Function,
   Normal,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Walker<'a> {
   input: &'a str,
-  part: Vec<Rc<Part>>,
+  part: Part<'a>,
   current_type: WalkingType,
 }
 
@@ -63,58 +65,61 @@ impl <'a>Walker<'a> {
   fn new(input: &'a str) -> Walker {
     Walker {
       input: input,
-      part: Vec::new(),
+      part: Part::new(AstType::Start, ' ', 0),
       current_type: WalkingType::Function,
     }
   }
 
-  pub fn get_next_target(last_part: &mut Vec<Rc<Part>>, index: usize) -> &mut Vec<Rc<Part>> {
+  pub fn get_next_target(last_part: &'a mut Part<'a>, index: usize) -> &'a mut Part<'a> {
     if index == 0 {
       return last_part;
     }
 
     let last_index = index - 1;
-    let last_char = last_part[last_index].value;
+    let last_char = last_part.value;
+
     if last_char == '{' {
-      return &mut Rc::get_mut(&mut last_part[last_index]).unwrap().children;
+      // last_part.add_child(AstType::Start, '{', 0, last_part);
+      return Rc::get_mut(&mut last_part.children[index]).unwrap();
     } else if last_char == '}' {
+      // let def = last_part.parent.upgrade().unwrap();
+      return &mut Rc::get_mut(&mut last_part.parent.upgrade().unwrap()).unwrap();
 
     }
 
     return last_part;
   }
 
-  pub fn walk(&mut self) {
-    let mut chars = self.input.chars();
-    let mut index = 0;
+  pub fn walk(&'_ mut self) {
+    // let mut chars = self.input.chars();
+    // let mut index = 0;
     
-    let target = &mut self.part;
-    while let Some(cha) = chars.next() {
-      let mut target = Walker::get_next_target(target, index);
-      match cha {
-        '{' => {
-          target.push(Rc::new(Part::new(AstType::Start, cha, index)));
-          continue;
-        },
-        '}' => {
-          target.push(Rc::new(Part::new(AstType::End, cha, index)));
-        },
-        ' ' => {
-          target.push(Rc::new(Part::new(AstType::End, cha, index)));
-        },
-        _ => {
-          target.push(Rc::new(Part::new(AstType::Normal, cha, index)));
-        }
-      };
-      index += 1;
-    }
-    println!("{:?}", target);
+    // let target = &mut self.part;
+    // while let Some(cha) = chars.next() {
+    //   let mut target = Walker::get_next_target(&mut target, index);
+    //   match cha {
+    //     '{' => {
+    //       target.children.push(Rc::new(Part::new(AstType::Start, cha, index)));
+    //       continue;
+    //     },
+    //     '}' => {
+    //       target.children.push(Rc::new(Part::new(AstType::End, cha, index)));
+    //     },
+    //     ' ' => {
+    //       target.children.push(Rc::new(Part::new(AstType::End, cha, index)));
+    //     },
+    //     _ => {
+    //       target.children.push(Rc::new(Part::new(AstType::Normal, cha, index)));
+    //     }
+    //   };
+    //   index += 1;
+    // }
   }
 }
 
 fn main() {
   let mut walker = Walker::new("{a b  c}");
-  &walker.walk();
+  walker.walk();
 
   let nyn: String = "abc".to_string();
   println!("{}", nyn);
