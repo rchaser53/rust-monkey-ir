1f2ea2ad345357bf2129605919e66cc068b60fba
rchaser53 <tayoshizawa29@gmail.com>
2018-07-18 23:57:20 +0900

    try to create LlvmBuilder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate llvm_sys;
 
+use llvm_sys::*;
 use llvm_sys::core::*;
 use llvm_sys::target;
 use llvm_sys::analysis::{LLVMVerifyModule, LLVMVerifierFailureAction};
@@ -7,9 +8,20 @@ use llvm_sys::execution_engine::*;
 use std::ffi::CString;
 use std::os::raw::{c_char};
 
-/// Makes sure that the parts of LLVM we are going to use are
-/// initialised before we do anything with them.
-fn setup_llvm_builder() -> *mut llvm_sys::LLVMBuilder {
+struct LlvmBuilder {
+  builder: *mut LLVMBuilder
+}
+
+impl LlvmBuilder {
+  fn new() -> LlvmBuilder {
+    LlvmBuilder::initialise();
+
+    LlvmBuilder {
+      builder: unsafe { LLVMCreateBuilder() }
+    }
+  }
+
+  fn initialise() {
     unsafe {
         if target::LLVM_InitializeNativeTarget() != 0 {
             panic!("Could not initialise target");
@@ -18,31 +30,34 @@ fn setup_llvm_builder() -> *mut llvm_sys::LLVMBuilder {
             panic!("Could not initialise ASM Printer");
         }
     }
+  }
 
-    unsafe { LLVMCreateBuilder() }
-}
-
-fn create_variable(builder: *mut llvm_sys::LLVMBuilder, name: &str, value: u64) -> *mut llvm_sys::LLVMValue {
+  fn create_variable(&mut self, name: &str, value: u64) -> *mut LLVMValue {
     let val_name = CString::new(name).unwrap();
     let llvm_value = unsafe {
-      LLVMBuildAlloca(builder, LLVMInt32Type(), val_name.as_ptr())
+      LLVMBuildAlloca(self.builder, LLVMInt32Type(), val_name.as_ptr())
     };
     unsafe {
-      LLVMBuildStore(builder, LLVMConstInt(LLVMInt32Type(), value, 0), llvm_value);
+      LLVMBuildStore(self.builder, LLVMConstInt(LLVMInt32Type(), value, 0), llvm_value);
     }
 
     llvm_value
+  }
+
+  fn dump(&self, module: *mut LLVMModule) {
+    unsafe { LLVMDumpModule(module) }
+  }
 }
 
 fn main() {
     let llvm_error = 1;
-
-    let builder = setup_llvm_builder();
+    
+    let mut llvm_builder = LlvmBuilder::new();
+    let builder = llvm_builder.builder;
 
     let mod_name = CString::new("my_module").unwrap();
     let module = unsafe { LLVMModuleCreateWithName(mod_name.as_ptr()) };
 
-    // create our function prologue
     let function_type = unsafe {
         let mut param_types = [];
         LLVMFunctionType(LLVMInt32Type(), param_types.as_mut_ptr(), param_types.len() as u32, 0)
@@ -53,17 +68,16 @@ fn main() {
     let entry_block = unsafe { LLVMAppendBasicBlock(function, entry_name.as_ptr()) };
     unsafe { LLVMPositionBuilderAtEnd(builder, entry_block); }
 
-    let a = create_variable(builder, "a", 35);
-    let b = create_variable(builder, "b", 16);
+    let a = llvm_builder.create_variable("a", 35);
+    let b = llvm_builder.create_variable("b", 16);
 
-    // return a + b
     let b_val_name = CString::new("b_val").unwrap();
     let b_val = unsafe { LLVMBuildLoad(builder, b, b_val_name.as_ptr()) };
     let a_val_name = CString::new("a_val").unwrap();
     let a_val = unsafe { LLVMBuildLoad(builder, a, a_val_name.as_ptr()) };
     let ab_val_name = CString::new("ab_val").unwrap();
     unsafe {
-        let res = LLVMBuildAdd(builder, a_val, b_val, ab_val_name.as_ptr());
+        let res = LLVMBuildMul(builder, a_val, b_val, ab_val_name.as_ptr());
         LLVMBuildRet(builder, res);
     }
 
@@ -81,8 +95,7 @@ fn main() {
     // Clean up the builder now that we are finished using it.
     unsafe { LLVMDisposeBuilder(builder) }
 
-    // Dump the LLVM IR to stdout so we can see what we've created
-    unsafe { LLVMDumpModule(module) }
+    llvm_builder.dump(module);
 
     // create our exe engine
     let mut engine: LLVMExecutionEngineRef = 0 as LLVMExecutionEngineRef;
