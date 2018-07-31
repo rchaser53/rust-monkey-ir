ea5cbf4bebc2ed7b0ed3b3a73031657632f7722f
rchaser53 <tayoshizawa29@gmail.com>
2018-07-31 22:51:44 +0900

    add memo to know llvm_ir format

--- play_llvm_ir/hoge.c
+++ play_llvm_ir/hoge.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-  int a = 3;
-  int b = 4;
+const char * hoge() {
+  return "hongya";
+}
 
-  printf("%d", a + b);
+int main(int argc, char **argv) {
+  hoge();
 }
