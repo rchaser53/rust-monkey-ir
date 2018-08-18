d628bdb6d71fdb192b23f3d2122b1348ddbe4558
rchaser53 <tayoshizawa29@gmail.com>
2018-08-18 23:26:05 +0900

    wanna know higher order function in llvm

--- play_llvm_ir/hoge.c
+++ play_llvm_ir/hoge.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
-const char * hoge() {
-  return "hongya";
+int square(int n) {
+  return n * n;
+}
+
+void map(const int *source, int *result, size_t n, int (*func)(int)) {
+  int i;
+
+  for (i = 0; i < n; i++) {
+    result[i] = func(source[i]);
+  }
 }
 
 int main(int argc, char **argv) {
-  hoge();
+  int numbers[] = {1, 2, 3, 4, 5};
+  int result[5] = {0};
+
+  map(numbers, result, 5, (int (*)(int))square);
+
+  int i = 0;
+  for (i = 0; i < 5; i++) {
+    printf("%d\n", result[i]);
+  }
 }
