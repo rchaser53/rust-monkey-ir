8e92e064e72c8cd13d9221d86a766724083766ff
rchaser53 <tayoshizawa29@gmail.com>
2018-10-10 22:19:40 +0900

    add variable llvm ir test

--- src/ir/arithmetic.rs
+++ src/ir/arithmetic.rs
@@ -6,15 +6,6 @@ use llvm_sys::*;
 use ir::builder::*;
 use ir::llvm_type::*;
 
-pub fn multiple_variable(
-    builder: *mut LLVMBuilder,
-    var_a: *mut LLVMValue,
-    var_b: *mut LLVMValue,
-    name: &str,
-) -> *mut LLVMValue {
-    unsafe { LLVMBuildMul(builder, var_a, var_b, c_string!(name).as_ptr()) }
-}
-
 pub fn add_variable(
     builder: *mut LLVMBuilder,
     var_a: *mut LLVMValue,
@@ -33,9 +24,25 @@ pub fn sub_variable(
     name: &str,
 ) -> *mut LLVMValue {
     unsafe { LLVMBuildSub(builder, var_a, var_b, c_string!(name).as_ptr()) }
-} 
+}
 
+pub fn multiple_variable(
+    builder: *mut LLVMBuilder,
+    var_a: *mut LLVMValue,
+    var_b: *mut LLVMValue,
+    name: &str,
+) -> *mut LLVMValue {
+    unsafe { LLVMBuildMul(builder, var_a, var_b, c_string!(name).as_ptr()) }
+}
 
+#[warn(dead_code)]
+fn setup_llvm() -> LlvmBuilder {
+    unsafe {
+      let mut lb = LlvmBuilder::new("test_module");
+      lb.setup_main();
+      lb
+    }
+}
 
 #[warn(dead_code)]
 fn int_arithmetic_assert(actual: *mut LLVMValue, expect: *mut LLVMValue) {
@@ -52,8 +59,14 @@ fn int_arithmetic_assert(actual: *mut LLVMValue, expect: *mut LLVMValue) {
 #[test]
 fn try_to_know_how_to_test_ir() {
     unsafe {
+      let lb = setup_llvm();
       int_arithmetic_assert(
-          LLVMConstInt(int32_type(), 3, 0),
+          add_variable(
+            lb.builder,
+            LLVMConstInt(int32_type(), 1, 0),
+            LLVMConstInt(int32_type(), 2, 0),
+            ""
+          ),
           LLVMConstInt(int32_type(), 3, 0)
       );
     }
