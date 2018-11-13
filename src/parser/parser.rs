81e136bc40321efe07f826ed32f6bde2adaa9820
rchaser53 <tayoshizawa29@gmail.com>
2018-11-13 20:03:31 +0900

    fix infix expression type error

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -13,7 +13,25 @@ pub fn get_expression_llvm_type(expression: &Expression) -> LLVMExpressionType {
         Expression::Array(expression_type, elements) => {
             LLVMExpressionType::Array(Box::new(expression_type), elements.len() as u32)
         }
+        Expression::ArrayChild(_, boxed_element, _) => get_expression_llvm_type(&boxed_element),
+        Expression::Infix(infix, left, right, _) => handle_infix_type(infix, *left),
         _ => LLVMExpressionType::Null,
+    }
+}
+
+pub fn handle_infix_type(infix: Infix, left: Expression) -> LLVMExpressionType {
+    match infix {
+        Infix::Plus => get_expression_llvm_type(&left),
+        Infix::Minus => LLVMExpressionType::Int,
+        Infix::Divide => LLVMExpressionType::Int,
+        Infix::Multiply => LLVMExpressionType::Int,
+        Infix::Rem => LLVMExpressionType::Int,
+        Infix::Eq => LLVMExpressionType::Boolean,
+        Infix::NotEq => LLVMExpressionType::Boolean,
+        Infix::Gte => LLVMExpressionType::Boolean,
+        Infix::Gt => LLVMExpressionType::Boolean,
+        Infix::Lte => LLVMExpressionType::Boolean,
+        Infix::Lt => LLVMExpressionType::Boolean,
     }
 }
 
